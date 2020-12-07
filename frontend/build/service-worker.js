d9ca747688072a28df6983e9976c9ef22e6da1e1
feuerfrie1 <61232931+feuerfrie1@users.noreply.github.com>
2020-12-07 19:57:51 +0300

    Add files via upload

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.cfb7b4fbd320b3705b798785a8b817d1.js"
+  "./precache-manifest.91fc654802cf2479437acb0f960689a0.js"
 );
 
 self.addEventListener('message', (event) => {
